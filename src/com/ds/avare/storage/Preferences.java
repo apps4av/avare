479b3601ac0cc0fe1e1d9c39ac7f736152fb437d
Zubair Khan <zkhan@zubair-laptop.(none)>
2013-09-11 23:25:42 -0400

    jpg

--- src/com/ds/avare/storage/Preferences.java
+++ src/com/ds/avare/storage/Preferences.java
@@ -39,7 +39,7 @@ public class Preferences {
     public static String distanceConversionUnit = "nm";
     public static String speedConversionUnit = "kt";
 
-    public static final String IMAGE_EXTENSION = ".png";
+    public static final String IMAGE_EXTENSION = ".jpg";
     
     /*
      * MAX number of elements
