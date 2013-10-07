25dec79581d52a51e457bc5e4a98c9c9f859f174
Zubair Khan <zkhan@zubair-laptop.(none)>
2013-10-06 20:43:22 -0400

    removed adsb support due to lack of ability to test

--- src/com/ds/avare/storage/Preferences.java
+++ src/com/ds/avare/storage/Preferences.java
@@ -336,7 +336,7 @@ public class Preferences {
      * @return
      */
     public boolean useAdsb() {
-        return(mPref.getBoolean(mContext.getString(R.string.adsb), false));
+        return(false);
     }
 
     /**
