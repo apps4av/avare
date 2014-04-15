7d09e6c507a3872ce148836dc2ec06522b516e73
Zubair Khan <zkhan@zubair-laptop.(none)>
2014-04-14 20:23:46 -0400

    5.5.2

--- src/com/ds/avare/IHelperService.java
+++ src/com/ds/avare/IHelperService.java
@@ -146,7 +146,7 @@ public class IHelperService extends Service {
                     }
                     if(c != null) {
                         deviation = c.getDeviation();
-                        if(c.isLeft()) {
+                        if(!c.isLeft()) {
                             deviation = -deviation;
                         }
                     }
