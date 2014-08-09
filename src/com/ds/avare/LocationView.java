661dcaed336a5b3a778429f7fe6dd923efef4f90
Zubair Khan <zkhan@zubair-laptop.(none)>
2014-08-08 23:15:13 -0400

    fixed Nexrad. Temporary fix.

--- src/com/ds/avare/LocationView.java
+++ src/com/ds/avare/LocationView.java
@@ -59,8 +59,6 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
 import android.graphics.Point;
-import android.graphics.Rect;
-import android.graphics.RectF;
 import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Handler;
