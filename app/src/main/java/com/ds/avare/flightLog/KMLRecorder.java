8136748f909d1dd7d9d641dcd076c331c82bfa92
annerajb <annerjb@asd.com>
2015-08-25 08:47:47 -0400

    forgot the include

--- app/src/main/java/com/ds/avare/flightLog/KMLRecorder.java
+++ app/src/main/java/com/ds/avare/flightLog/KMLRecorder.java
@@ -25,7 +25,6 @@ import java.util.TimeZone;
 
 import android.annotation.SuppressLint;
 import android.os.Environment;
-import android.util.Log;
 
 import com.ds.avare.gps.GpsParams;
 import com.ds.avare.shapes.CrumbsShape;
