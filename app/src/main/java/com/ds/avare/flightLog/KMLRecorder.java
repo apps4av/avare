d4342d77e90f0a2bf6d0c593bbfa1155cbd857e1
annerajb <annerajb@users.noreply.github.com>
2015-08-30 16:38:26 -0400

    Updated author

--- app/src/main/java/com/ds/avare/flightLog/KMLRecorder.java
+++ app/src/main/java/com/ds/avare/flightLog/KMLRecorder.java
@@ -1,7 +1,7 @@
 /*
 Copyright (c) 2013, Avare software (apps4av.com) 
 All rights reserved.
-
+@author annerajb
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
     * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
