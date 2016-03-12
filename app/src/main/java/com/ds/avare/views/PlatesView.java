8f3017aeeb5063789d1c2c18eeb7df5a984c3983
Ryan O'Leary <roleary@gmail.com>
2016-03-12 13:56:48 -0800

    Remove logging

--- app/src/main/java/com/ds/avare/views/PlatesView.java
+++ app/src/main/java/com/ds/avare/views/PlatesView.java
@@ -18,7 +18,6 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
@@ -413,7 +412,6 @@ public class PlatesView extends View implements OnTouchListener {
             // Don't pan/draw if multi-touch scaling is under way
             if( mScaling ) return false;
 
-            Log.i("PlatesView::GestureList", "mDraw: " + mDraw);
             /*
              * Do not move on multitouch
              */
