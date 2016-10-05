2f188e95b7d7282d7cae90fc02b887a48dfe0f1f
apps4av <apps4av@gmail.com>
2016-10-05 11:59:11 -0400

    change of met colors and color met text

--- app/src/main/java/com/ds/avare/views/LocationView.java
+++ app/src/main/java/com/ds/avare/views/LocationView.java
@@ -1207,10 +1207,7 @@ public class LocationView extends View implements MultiTouchObjectCanvas<Object>
                         /*
                          * Set MET tfr
                          */
-                        String txt = cshape.getTextIfTouched(lon, lat);
-                        if(null != txt) {
-                            textMets += txt + "\n--\n";
-                        }
+                        textMets += cshape.getHTMLMetOnTouch(mContext, mets.get(i), lon, lat);
                     }
                 }
             }            
