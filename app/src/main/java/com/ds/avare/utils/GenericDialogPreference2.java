4351e7c6bb8585870b40718a28172d381d308722
Ahsan Rabbani <arabbani@twilio.com>
2016-10-28 20:08:31 -0700

    some button changes

---
+++ app/src/main/java/com/ds/avare/utils/GenericDialogPreference2.java
@@ -0,0 +1,16 @@
+package com.ds.avare.utils;
+
+import android.content.Context;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+
+/**
+ * Created by arabbani on 10/24/16.
+ */
+public class GenericDialogPreference2 extends DialogPreference {
+
+    public GenericDialogPreference2(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
