ad325462797fedec092fe02113d8be29d2939325
apps4av <apps4av@gmail.com>
2017-02-12 15:01:47 -0500

    release 7.6.7

--- app/src/main/java/com/ds/avare/utils/BitmapHolder.java
+++ app/src/main/java/com/ds/avare/utils/BitmapHolder.java
@@ -12,16 +12,17 @@ Redistribution and use in source and binary forms, with or without modification,
 
 package com.ds.avare.utils;
 
-import java.io.File;
-
-import com.ds.avare.position.Origin;
-import com.ds.avare.storage.Preferences;
-
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
+import android.graphics.Rect;
+
+import com.ds.avare.position.Origin;
+import com.ds.avare.storage.Preferences;
+
+import java.io.File;
 
 /**
  * @author zkhan
@@ -86,7 +87,6 @@ public class BitmapHolder {
 
     /**
      * @param
-     * Get bitmap from renderer
      */
     public BitmapHolder() {
         Bitmap.Config conf = Bitmap.Config.RGB_565;
@@ -104,7 +104,7 @@ public class BitmapHolder {
 
     /**
      * @param
-     * Get bitmap from renderer of type
+     *
      */
     public BitmapHolder(Bitmap.Config config) {
         try {
@@ -120,7 +120,7 @@ public class BitmapHolder {
     }
 
     /**
-     * @param name
+     *
      * Get bitmap from renderer
      */
     public BitmapHolder(int width, int height) {
@@ -158,6 +158,20 @@ public class BitmapHolder {
         }
         mTransform.setTranslate(x, y);
         mCanvas.drawBitmap(b.getBitmap(), mTransform, null);
+    }
+
+    /**
+     *
+     * @param b is bitmap to draw
+     */
+    public void drawInBitmap(BitmapHolder b, Rect src, Rect dst) {
+        if((null == b) || (null == mCanvas)) {
+            return;
+        }
+        if(null == b.getBitmap()) {
+            return;
+        }
+        mCanvas.drawBitmap(b.getBitmap(), src, dst, null);
     }
 
     /**
@@ -408,7 +422,7 @@ public class BitmapHolder {
     
     /**
      * 
-     * @param nothing in it?
+     * @param
      */
     public void setFound(boolean found) {
         mFound = found;
@@ -416,7 +430,7 @@ public class BitmapHolder {
     
     /**
      * 
-     * @param nothing in it?
+     * @param
      */
     public boolean getFound() {
         return mFound;
