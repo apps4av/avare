639f626855dd79bd39ba2e0fd1c704a56d589087
jackwilliard <jackfrakn@aol.com>
2020-09-03 18:44:20 -0400

    Closes #368
    The current preferences lack a persistent value ofr other info. Users request this capability to handle additional FAA required information.
    
    This commit adds a preference that persistently stores "other info" box for the user.

--- app/src/main/java/com/ds/avare/plan/LmfsPlan.java
+++ app/src/main/java/com/ds/avare/plan/LmfsPlan.java
@@ -151,6 +151,7 @@ public class LmfsPlan {
         noOfAircraft = "1";
         wakeTurbulence = "LIGHT";
         route = DIRECT;
+        otherInfo = pref.getAircraftOtherInfo();
 		surveillanceEquipment = pref.getAircraftSurveillanceEquipment();
         aircraftEquipment = pref.getAircraftEquipment();
         cruisingSpeed = String.valueOf(pref.getAircraftTAS());
