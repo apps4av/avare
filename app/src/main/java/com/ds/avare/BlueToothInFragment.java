c8e239e4ce671844dc77364bfee0bd967e2125b2
Ron Walker <n520tx@gmail.com>
2022-09-02 14:36:40 -0500

    Clear out the "lastconnected" value when the connection is DISconnected.

--- app/src/main/java/com/ds/avare/BlueToothInFragment.java
+++ app/src/main/java/com/ds/avare/BlueToothInFragment.java
@@ -93,9 +93,11 @@ public class BlueToothInFragment extends Fragment {
                 /*
                  * If connected, disconnect
                  */
+                Preferences pref = new Preferences(getActivity());
                 if (mBt.isConnected()) {
                     mBt.stop();
                     mBt.disconnect();
+                    pref.setLastConnectedBtIn(null); //clear
                     setStates();
                     return;
                 }
@@ -107,13 +109,9 @@ public class BlueToothInFragment extends Fragment {
                     mConnectButton.setText(getString(R.string.Connect));
                     mBt.setHelper(((IOActivity)getActivity()).getService());
                     mBt.connect(val, mSecureCb.isChecked());
-                    Preferences pref = new Preferences(getActivity());
                     if (mBt.isConnected()) {
                         mBt.start(pref);
                         pref.setLastConnectedBtIn(val); // save where we connected
-                    }
-                    else {
-                        pref.setLastConnectedBtIn(null); //clear
                     }
                     setStates();
                 }
